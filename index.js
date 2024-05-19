2a4f975c2282749da00580ece36a5b4b7f4ccf4e
Mk47q <mtynkarjw@gmail.com>
2024-05-18 21:37:15 -0700

    Start commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let menubutton = document.querySelector(".menu-button")
+let menubar = document.querySelector(".menu-bar")
+let backmenu = document.querySelector(".back-menu")
+menubutton.addEventListener("click", function(){
+    menubar.classList.toggle("open");
+    backmenu.classList.toggle("show");
+})
+backmenu.addEventListener("click", function(){
+    menubar.classList.remove("open")
+    backmenu.classList.remove("show")
+    inpsearch.style.display = "none"
+})
+let searchbutton = document.querySelector(".search-button")
+let inpsearch = document.querySelector(".inp-search")
+searchbutton.addEventListener("click", function(){
+    inpsearch.style.display = "block"
+})
