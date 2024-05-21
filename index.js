3c8ce0ae6eef879bd7b50f0606223a808f88e5d3
Mk47q <mtynkarjw@gmail.com>
2024-05-20 23:37:32 -0700

    add slide commit

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ let backmenu = document.querySelector(".back-menu")
 menubutton.addEventListener("click", function(){
     menubar.classList.toggle("open");
     backmenu.classList.toggle("show");
-})
+});
 backmenu.addEventListener("click", function(){
     menubar.classList.remove("open")
     backmenu.classList.remove("show")
     inpsearch.style.display = "none"
-})
+});
 let searchbutton = document.querySelector(".search-button")
 let inpsearch = document.querySelector(".inp-search")
 searchbutton.addEventListener("click", function(){
     inpsearch.style.display = "block"
-})
+});
